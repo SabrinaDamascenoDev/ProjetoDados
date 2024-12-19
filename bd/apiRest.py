cd358cc25b1248b244f3ad05eca079bab7afdbf7
SabrinaDamascenoDev <sabrinadamascenodev@gmail.com>
2024-12-19 18:45:02 -0300

    Finalizando apiRest, só com os metodos get

--- bd/apiRest.py
+++ bd/apiRest.py
@@ -8,12 +8,40 @@ def faturamento():
     faturamento_list = list(faturamento)
 
     if faturamento_list:
-        for item in faturamento_list:
-            print(item)
-        return jsonify(faturamento_list)
+        return jsonify(faturamento_list), 200
     else:
         print("Nenhum documento encontrado.")
-        return jsonify({"message": "Nenhum documento encontrado."})
+        return jsonify({"message": "Nenhum documento encontrado."}), 404
+
+@app.route('/lojas', methods=['GET'])
+def lojas():
+    lojas = collection.find({}, {'ID Loja': 1, '_id': 0})
+    lojas_list = list(lojas)
+    if lojas_list:
+        return jsonify(lojas_list), 200
+    else:
+        print("Nenhum documento encontrado.")
+        return jsonify({"message": "Nenhum documento encontrado."}), 404
+
+@app.route('/quantidade', methods=['GET'])
+def quantidade():
+    quantidade = collection2.find({}, {'Quantidade': 1, '_id':0})
+    quantidade_list = list(quantidade)
+    if quantidade_list:
+        return jsonify(quantidade_list), 200
+    else:
+        print("Nenhum documento encontrado.")
+        return jsonify({"message": "Nenhum documento encontrado."}), 404
+
+@app.route('/ticket', methods=['GET'])
+def ticket():
+    ticket = collection3.find({}, {'Ticket Médio': 1, '_id': 0})
+    quantidade_list = list(ticket)
+    if quantidade_list:
+        return jsonify(quantidade_list), 200
+    else:
+        print("Nenhum documento encontrado.")
+        return jsonify({"message": "Nenhum documento encontrado."}), 404
 
 
 if __name__ == '__main__':
